72d502ec1575ac1e50315bf22d226895fbd680cd
AgosBC <agostinabarello@gmail.com>
2021-09-16 21:26:04 -0300

    creacion de entities

---
+++ src/test/java/ar/com/ada/mongo/metflix/MetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ada.mongo.metflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
